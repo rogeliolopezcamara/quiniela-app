e53c0a5f1d38432393ace544ce8416383d0d7fe2
Rogelio Lopez Camara <rogeliolopezcamara@MacBook-Air-de-Rogelio-2.local>
2025-07-19 10:04:43 -0500

    Ajuste menú horizontal

--- quiniela-frontend/src/components/Sidebar.jsx
+++ quiniela-frontend/src/components/Sidebar.jsx
@@ -125,7 +125,7 @@ function Sidebar() {
       )}
 
       {isMobile && (
-        <nav className="fixed bottom-12 left-0 right-0 bg-white border-t border-gray-300 shadow-md z-50 flex justify-around py-2">
+        <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-300 shadow-md z-50 flex justify-around h-20 pt-2">
           <button
             onClick={() => navigate("/")}
             className="flex flex-col items-center text-xs"
