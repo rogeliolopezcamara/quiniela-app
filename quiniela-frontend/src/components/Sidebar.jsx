ead27f0cf593a2d7acab45895e8432b061c2e854
Rogelio Lopez Camara <rogeliolopezcamara@MacBook-Air-de-Rogelio-2.local>
2025-07-18 23:42:26 -0500

    Ajuste menú inferior 2

--- quiniela-frontend/src/components/Sidebar.jsx
+++ quiniela-frontend/src/components/Sidebar.jsx
@@ -125,7 +125,7 @@ function Sidebar() {
       )}
 
       {isMobile && (
-        <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-300 shadow-md z-50 flex justify-around pt-2 pb-1">
+        <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-300 shadow-md z-50 flex justify-around pt-2 pb-2">
           <button
             onClick={() => navigate("/")}
             className="flex flex-col items-center text-xs"
