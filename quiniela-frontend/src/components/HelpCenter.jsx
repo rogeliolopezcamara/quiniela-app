c50a5548811bf956ff4465889e4a29514ace1c04
Rogelio Lopez Camara <rogeliolopezcamara@MacBook-Air-de-Rogelio-2.local>
2025-07-18 22:25:03 -0500

    Hacer menú superior fijo 7

--- quiniela-frontend/src/components/HelpCenter.jsx
+++ quiniela-frontend/src/components/HelpCenter.jsx
@@ -12,7 +12,7 @@ const HelpCenter = () => {
       <Sidebar />
       <div className="pb-24 px-4 max-w-5xl mx-auto">
         <div className="sticky top-0 bg-white z-30 shadow-md py-1">
-          <div className="flex justify-center mb-8">
+          <div className="flex justify-center items-center h-full pt-3">
             <button
               className={`px-4 py-2 rounded-l-lg font-semibold border border-gray-300 transition-all duration-200 hover:bg-gray-100 ${
                 activeTab === "instructions" ? "bg-gray-200" : "bg-white"
