5a429ab0349768cca18dd582dbfc4389709cd163
Rogelio Lopez Camara <rogeliolopezcamara@MacBook-Air-de-Rogelio-2.local>
2025-07-18 22:10:57 -0500

    Hacer menú superior fijo 2

--- quiniela-frontend/src/components/HelpCenter.jsx
+++ quiniela-frontend/src/components/HelpCenter.jsx
@@ -11,24 +11,24 @@ const HelpCenter = () => {
     <>
       <Sidebar />
       <div className="pt-6 pb-24 px-4 max-w-5xl mx-auto">
-        <div className="sticky top-0 bg-white z-10">
+        <div className="sticky top-[4.5rem] bg-white z-10">
           <div className="flex justify-center mb-8">
-          <button
-            className={`px-4 py-2 rounded-l-lg font-semibold border border-gray-300 transition-all duration-200 hover:bg-gray-100 ${
-              activeTab === "instructions" ? "bg-gray-200" : "bg-white"
-            }`}
-            onClick={() => setActiveTab("instructions")}
-          >
-            Instrucciones
-          </button>
-          <button
-            className={`px-4 py-2 rounded-r-lg font-semibold border-t border-b border-r border-gray-300 transition-all duration-200 hover:bg-gray-100 ${
-              activeTab === "changelog" ? "bg-gray-200" : "bg-white"
-            }`}
-            onClick={() => setActiveTab("changelog")}
-          >
-            Novedades
-          </button>
+            <button
+              className={`px-4 py-2 rounded-l-lg font-semibold border border-gray-300 transition-all duration-200 hover:bg-gray-100 ${
+                activeTab === "instructions" ? "bg-gray-200" : "bg-white"
+              }`}
+              onClick={() => setActiveTab("instructions")}
+            >
+              Instrucciones
+            </button>
+            <button
+              className={`px-4 py-2 rounded-r-lg font-semibold border-t border-b border-r border-gray-300 transition-all duration-200 hover:bg-gray-100 ${
+                activeTab === "changelog" ? "bg-gray-200" : "bg-white"
+              }`}
+              onClick={() => setActiveTab("changelog")}
+            >
+              Novedades
+            </button>
           </div>
         </div>
 
