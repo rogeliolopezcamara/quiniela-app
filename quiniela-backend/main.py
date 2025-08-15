1ffacb0b1cfbef1cfc9a129e9e2adf5835021be5
Rogelio Lopez Camara <rogeliolopezcamara@MacBook-Air-de-Rogelio-2.local>
2025-08-15 17:25:05 -0400

    Ahora en la matriz en los partidos en vivo se ve el tiempo transcurrido 4-se agrega el status_elapsed en endpoint available-matches

--- quiniela-backend/main.py
+++ quiniela-backend/main.py
@@ -391,7 +391,11 @@ def get_available_matches_by_competition(
             "league_season": match.league_season,
             "league_round": match.league_round,
             "home_team_logo": match.home_team_logo,
-            "away_team_logo": match.away_team_logo
+            "away_team_logo": match.away_team_logo,
+            "status_long": match.status_long,
+            "status_short": match.status_short,
+            "status_elapsed": match.status_elapsed,
+            "status_extra": match.status_extra
         }
         for match in matches
     ]
